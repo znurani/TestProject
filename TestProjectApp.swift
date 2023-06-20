8ec32aa867285a002c0ed35a42653cd1736b46f8
znurani <129260668+znurani@users.noreply.github.com>
2023-06-19 21:11:13 -0600

    Initial commit

---
+++ TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Zeshan Nurani on 2023-06-19.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
